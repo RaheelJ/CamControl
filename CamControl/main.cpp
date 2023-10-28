5089e8e5eba086de2e673b6e3b63aeafc13928e3
RaheelJ <raheeljaved_ee@yahoo.com>
2023-10-28 17:13:18 -0400

    Before merge

--- CamControl/main.cpp
+++ CamControl/main.cpp
@@ -72,7 +72,8 @@ int main()
 	service_list[0].packet_size = 247;
 	service_list[0].operation = READ;
 
-	bt_link::source bt_source;
+	auto temp_adapters = SimpleBLE::Adapter::get_adapters();
+	/*bt_link::source bt_source;
 	bt_source.Initialize(source_config);
 	
 	bt_link::node bt_node;
@@ -80,7 +81,7 @@ int main()
 	bt_source.Add_Node(bt_node);
 
 	std::string in_data, out_data;
-	bt_source.Use_Service(bt_node.Get_Address(), service_list[0], in_data, out_data);
+	bt_source.Use_Service(bt_node.Get_Address(), service_list[0], in_data, out_data);*/
 	
 	return 0;
 }
